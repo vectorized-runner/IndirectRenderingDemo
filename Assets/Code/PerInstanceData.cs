c2e0253f16dca32cbabee149b93f01cb03908155
Bartu Baş <bartubas99@gmail.com>
2024-04-12 15:24:46 +0300

    Move the cubes using a Compute Shader

--- Assets/Code/PerInstanceData.cs
+++ Assets/Code/PerInstanceData.cs
@@ -1,6 +1,6 @@
 using Unity.Mathematics;
 
-// Should match the Shader layout exactly
+// Should match the Shader/Compute layout exactly
 public struct PerInstanceData
 {
 	public float4x4 LocalToWorld;
